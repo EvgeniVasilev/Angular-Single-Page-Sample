04685a1e8f9a634311dcdd3b7ccf5a3866d010a8
EvgeniVasilev <evgenivasilev1209@abv.bg>
2015-03-16 12:44:31 +0200

    Initial commit

---
+++ angular_api.js
@@ -0,0 +1,20 @@
+﻿function geographicalGlossary($scope, $http)
+{
+    var home = document.getElementById("home"),
+	     titles = document.getElementById("titles"),
+	     maps = document.getElementById("maps");
+    $scope.heading = "Angular Powered Geographical Glossary";
+    $http.get("body.js").success(function (data)
+    {
+        $scope.items = data;
+        titles.innerHTML = data[0].title;
+        home.innerHTML = data[0].description;
+        maps.src = data[0].img;
+    });
+    $scope.showItem = function (item, title, map)
+    {
+        home.innerHTML = item;
+        titles.innerHTML = title;
+        maps.src = map;
+    };
+}
